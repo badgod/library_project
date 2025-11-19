9507af3e95b3a541ce40ea8eb5cde440f1cdbbc3
Suphot Wongsri <w.suphot@outlook.com>
2025-11-20 00:27:09 +0700

    Initial commit

---
+++ backupdb/library_db_2025-11-19.sql
@@ -0,0 +1,419 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 19, 2025 at 07:26 AM
+-- Server version: 8.0.40
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_db`
+--
+CREATE DATABASE IF NOT EXISTS `library_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `library_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_title`
+--
+
+DROP TABLE IF EXISTS `book_title`;
+CREATE TABLE `book_title` (
+  `title_id` int NOT NULL COMMENT 'รหัสชื่อเรื่อง (Primary Key)',
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อเรื่อง',
+  `author` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ผู้แต่ง',
+  `isbn` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ISBN (รหัสมาตรฐานสากลประจำหนังสือ)',
+  `category_id` int DEFAULT NULL COMMENT 'รหัสหมวดหมู่'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : ชื่อเรื่องหนังสือหลัก';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `category_id` int NOT NULL COMMENT 'รหัสหมวดหมู่ (Primary Key)',
+  `category_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อหมวดหมู่ เช่น เทคโนโลยี การบริหาร'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : หมวดหมู่';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ebook`
+--
+
+DROP TABLE IF EXISTS `ebook`;
+CREATE TABLE `ebook` (
+  `ebook_id` int NOT NULL COMMENT 'รหัส E-Book (Primary Key)',
+  `title_id` int DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
+  `ebook_file` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ไฟล์ ebook'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : หนังสืออิเล็กทรอนิกส์';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ebook_log`
+--
+
+DROP TABLE IF EXISTS `ebook_log`;
+CREATE TABLE `ebook_log` (
+  `ebooklog_id` int NOT NULL COMMENT 'รหัสการเก็บ',
+  `ebook_id` int DEFAULT NULL COMMENT 'รหัส E-book',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก',
+  `ebooklog_date` timestamp NULL DEFAULT NULL COMMENT 'วันเวลาที่เปิดอ่าน'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : บันทึกการอ่าน E-Book';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fine`
+--
+
+DROP TABLE IF EXISTS `fine`;
+CREATE TABLE `fine` (
+  `fine_id` int NOT NULL COMMENT 'รหัสการปรับ',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
+  `fine_amount` decimal(8,2) DEFAULT NULL COMMENT 'จำนวนเงินที่ปรับ',
+  `incurred_date` date DEFAULT NULL COMMENT 'วันที่เริ่มคิดค่าปรับ',
+  `paid_date` datetime DEFAULT NULL COMMENT 'วันที่มาชำระเงิน'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : ค่าปรับ';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+CREATE TABLE `loan` (
+  `loan_id` int NOT NULL COMMENT 'รหัสการยืม',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก',
+  `loan_date` date DEFAULT NULL COMMENT 'วันที่ยืม',
+  `due_date` date DEFAULT NULL COMMENT 'วันครบกำหนดคืน',
+  `return_date` date DEFAULT NULL COMMENT 'วันที่คืนจริง',
+  `renewal_count` int DEFAULT '0' COMMENT 'จำนวนครั้งที่ต่ออายุ',
+  `status` enum('borrow','receive','somereturn','return') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (borrow ยืม, receive รับหนังสือ, somereturn คืนแล้วบางส่วน return คืนหนังสือ)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : การยืม';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan_item`
+--
+
+DROP TABLE IF EXISTS `loan_item`;
+CREATE TABLE `loan_item` (
+  `loan_item_id` int NOT NULL COMMENT 'รหัสรายละเอียดการยืม',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
+  `copy_id` int DEFAULT NULL COMMENT 'รหัสเล่มหนังสือ',
+  `status` enum('borrow','return') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'borrow' COMMENT 'สถานะ (ยืม, คืน)',
+  `return_date` date DEFAULT NULL COMMENT 'วันที่คืน'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : รายละเอียดการยืม';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan_renewal`
+--
+
+DROP TABLE IF EXISTS `loan_renewal`;
+CREATE TABLE `loan_renewal` (
+  `renewal_id` int NOT NULL COMMENT 'รหัสการต่ออายุ',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
+  `renewal_date` date DEFAULT NULL COMMENT 'วันที่ต่ออายุ',
+  `renewal_count_date` int DEFAULT NULL COMMENT 'จำนวนวันที่ขอต่ออายุ',
+  `return_date` date DEFAULT NULL COMMENT 'วันที่ต้องคืน'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : การต่ออายุ';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `member_id` int NOT NULL COMMENT 'รหัสสมาชิก (PK)',
+  `emloyee_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสพนักงาน',
+  `first_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อจริง',
+  `last_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'นามสกุล',
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'อีเมล (ใช้ติดต่อ)',
+  `tel` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'เบอร์โทรติดต่อ',
+  `staus` enum('active','inactive') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''active'', ''inactive'', ''pending'')'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : สมาชิก';
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`member_id`, `emloyee_id`, `first_name`, `last_name`, `email`, `tel`, `staus`) VALUES
+(1, '0000', 'ผู้ดูแลระบบ', 'เจ้าหน้าที่ห้องสมุด', 'admin@mail.com', '0999999999', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `physical_copy`
+--
+
+DROP TABLE IF EXISTS `physical_copy`;
+CREATE TABLE `physical_copy` (
+  `copy_id` int NOT NULL COMMENT 'รหัสฉบับเล่ม',
+  `title_id` int DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
+  `accession_no` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'เลขทะเบียน/รหัสประจำเล่ม (ใช้ติดตามการยืม-คืน)',
+  `status` enum('available','on_loan','lost') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''available'', ''on_loan'', ''lost'')'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง :  หนังสือที่เป็นเล่ม';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL COMMENT 'รหัสผู้ใช้งาน (PK)',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ชื่อผู้ใช้สำหรับ Login',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสผ่านที่ถูกเข้ารหัส',
+  `role` enum('member','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'สิทธิ์การใช้งาน (''member'', ''admin'')',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : ผู้ใช้ระบบ';
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `role`, `member_id`) VALUES
+(1, 'admin', '$2y$10$NB9oPlgek0SQ1HqQgfXKfusahEG6KnxIaVQX14jRbtRUVCi6NkTse', 'admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_title`
+--
+ALTER TABLE `book_title`
+  ADD PRIMARY KEY (`title_id`),
+  ADD KEY `book_title_category_FK` (`category_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `ebook`
+--
+ALTER TABLE `ebook`
+  ADD PRIMARY KEY (`ebook_id`),
+  ADD KEY `ebook_book_title_FK` (`title_id`);
+
+--
+-- Indexes for table `ebook_log`
+--
+ALTER TABLE `ebook_log`
+  ADD PRIMARY KEY (`ebooklog_id`),
+  ADD KEY `ebook_log_ebook_FK` (`ebook_id`),
+  ADD KEY `ebook_log_member_FK` (`member_id`);
+
+--
+-- Indexes for table `fine`
+--
+ALTER TABLE `fine`
+  ADD PRIMARY KEY (`fine_id`),
+  ADD KEY `fine_loan_FK` (`loan_id`);
+
+--
+-- Indexes for table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`loan_id`),
+  ADD KEY `loan_member_FK` (`member_id`);
+
+--
+-- Indexes for table `loan_item`
+--
+ALTER TABLE `loan_item`
+  ADD PRIMARY KEY (`loan_item_id`),
+  ADD KEY `loan_item_loan_FK` (`loan_id`),
+  ADD KEY `loan_item_physical_copy_FK` (`copy_id`);
+
+--
+-- Indexes for table `loan_renewal`
+--
+ALTER TABLE `loan_renewal`
+  ADD PRIMARY KEY (`renewal_id`),
+  ADD KEY `loan_renewal_loan_FK` (`loan_id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`member_id`),
+  ADD UNIQUE KEY `member_unique` (`emloyee_id`);
+
+--
+-- Indexes for table `physical_copy`
+--
+ALTER TABLE `physical_copy`
+  ADD PRIMARY KEY (`copy_id`),
+  ADD UNIQUE KEY `physical_copy_unique` (`accession_no`),
+  ADD KEY `physical_copy_book_title_FK` (`title_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `user_member_FK` (`member_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_title`
+--
+ALTER TABLE `book_title`
+  MODIFY `title_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสชื่อเรื่อง (Primary Key)';
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสหมวดหมู่ (Primary Key)';
+
+--
+-- AUTO_INCREMENT for table `ebook`
+--
+ALTER TABLE `ebook`
+  MODIFY `ebook_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัส E-Book (Primary Key)';
+
+--
+-- AUTO_INCREMENT for table `ebook_log`
+--
+ALTER TABLE `ebook_log`
+  MODIFY `ebooklog_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการเก็บ';
+
+--
+-- AUTO_INCREMENT for table `fine`
+--
+ALTER TABLE `fine`
+  MODIFY `fine_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการปรับ';
+
+--
+-- AUTO_INCREMENT for table `loan`
+--
+ALTER TABLE `loan`
+  MODIFY `loan_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการยืม';
+
+--
+-- AUTO_INCREMENT for table `loan_item`
+--
+ALTER TABLE `loan_item`
+  MODIFY `loan_item_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสรายละเอียดการยืม';
+
+--
+-- AUTO_INCREMENT for table `loan_renewal`
+--
+ALTER TABLE `loan_renewal`
+  MODIFY `renewal_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการต่ออายุ';
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `member_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสสมาชิก (PK)', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `physical_copy`
+--
+ALTER TABLE `physical_copy`
+  MODIFY `copy_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสฉบับเล่ม';
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสผู้ใช้งาน (PK)', AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book_title`
+--
+ALTER TABLE `book_title`
+  ADD CONSTRAINT `book_title_category_FK` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+
+--
+-- Constraints for table `ebook`
+--
+ALTER TABLE `ebook`
+  ADD CONSTRAINT `ebook_book_title_FK` FOREIGN KEY (`title_id`) REFERENCES `book_title` (`title_id`);
+
+--
+-- Constraints for table `ebook_log`
+--
+ALTER TABLE `ebook_log`
+  ADD CONSTRAINT `ebook_log_ebook_FK` FOREIGN KEY (`ebook_id`) REFERENCES `ebook` (`ebook_id`),
+  ADD CONSTRAINT `ebook_log_member_FK` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+
+--
+-- Constraints for table `fine`
+--
+ALTER TABLE `fine`
+  ADD CONSTRAINT `fine_loan_FK` FOREIGN KEY (`loan_id`) REFERENCES `loan` (`loan_id`);
+
+--
+-- Constraints for table `loan`
+--
+ALTER TABLE `loan`
+  ADD CONSTRAINT `loan_member_FK` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+
+--
+-- Constraints for table `loan_item`
+--
+ALTER TABLE `loan_item`
+  ADD CONSTRAINT `loan_item_loan_FK` FOREIGN KEY (`loan_id`) REFERENCES `loan` (`loan_id`),
+  ADD CONSTRAINT `loan_item_physical_copy_FK` FOREIGN KEY (`copy_id`) REFERENCES `physical_copy` (`copy_id`);
+
+--
+-- Constraints for table `loan_renewal`
+--
+ALTER TABLE `loan_renewal`
+  ADD CONSTRAINT `loan_renewal_loan_FK` FOREIGN KEY (`loan_id`) REFERENCES `loan` (`loan_id`);
+
+--
+-- Constraints for table `physical_copy`
+--
+ALTER TABLE `physical_copy`
+  ADD CONSTRAINT `physical_copy_book_title_FK` FOREIGN KEY (`title_id`) REFERENCES `book_title` (`title_id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_member_FK` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ระบบจัดการห้องสมุด</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1><i class="fa-solid fa-book"></i> ระบบจัดการห้องสมุด (LMS)</h1>
+    </div>
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                1 of 2
+            </div>
+            <div class="col">
+                2 of 2
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                1 of 3
+            </div>
+            <div class="col">
+                2 of 3
+            </div>
+            <div class="col">
+                3 of 3
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@social</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
