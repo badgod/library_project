be998d47730559c96064a35d6b5206d5738a87ad
Suphot Wongsri <w.suphot@outlook.com>
2025-11-21 15:27:23 +0700

    1234

---
+++ config/sessionname.php
@@ -0,0 +1,2 @@
+<?php 
+session_name('library_app_session');
---
+++ includes/_footer.php
@@ -0,0 +1,12 @@
+ <footer class="container">
+     <p class="float-end"><a href="#">Back to top</a></p>
+     <p>
+         &copy; 2017–2025 Company, Inc. &middot;
+         <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
+     </p>
+ </footer>
+ </main>
+ <script src="vendor/bootstrap/js/bootstrap.bundle.min.js" class="astro-vvvwv3sm"></script>
+ </body>
+
+ </html>
