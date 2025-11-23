9058da85acf3358e02240593d5a3512ea8441a69
Suphot Wongsri <w.suphot@outlook.com>
2025-11-24 02:30:26 +0700

    Update library_db.sql

--- backupdb/library_db.sql
+++ backupdb/library_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 22, 2025 at 07:40 AM
--- Server version: 5.7.24
--- PHP Version: 8.3.1
+-- Host: localhost:8889
+-- Generation Time: Nov 23, 2025 at 07:29 PM
+-- Server version: 8.0.40
+-- PHP Version: 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,15 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `book_title`
 --
 
-DROP TABLE IF EXISTS `book_title`;
 CREATE TABLE `book_title` (
-  `title_id` int(11) NOT NULL COMMENT 'รหัสชื่อเรื่อง (Primary Key)',
-  `category_id` int(11) DEFAULT NULL COMMENT 'รหัสหมวดหมู่',
-  `title` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อเรื่อง',
-  `title_image` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รูปภาพหนังสือ',
-  `author` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ผู้แต่ง',
-  `isbn` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ISBN (รหัสมาตรฐานสากลประจำหนังสือ)',
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รายละเอียดหนังสือ'
+  `title_id` int NOT NULL COMMENT 'รหัสชื่อเรื่อง (Primary Key)',
+  `category_id` int DEFAULT NULL COMMENT 'รหัสหมวดหมู่',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อเรื่อง',
+  `image` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รูปภาพหนังสือ',
+  `author` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ผู้แต่ง',
+  `isbn` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ISBN (รหัสมาตรฐานสากลประจำหนังสือ)',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รายละเอียดหนังสือ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : ชื่อเรื่องหนังสือหลัก';
 
 -- --------------------------------------------------------
@@ -44,10 +43,10 @@ CREATE TABLE `book_title` (
 -- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
-  `category_id` int(11) NOT NULL COMMENT 'รหัสหมวดหมู่ (Primary Key)',
-  `category_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อหมวดหมู่ เช่น เทคโนโลยี การบริหาร'
+  `category_id` int NOT NULL COMMENT 'รหัสหมวดหมู่ (Primary Key)',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อหมวดหมู่ เช่น เทคโนโลยี การบริหาร',
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'คำอธิบาย'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : หมวดหมู่';
 
 -- --------------------------------------------------------
@@ -56,11 +55,10 @@ CREATE TABLE `category` (
 -- Table structure for table `ebook`
 --
 
-DROP TABLE IF EXISTS `ebook`;
 CREATE TABLE `ebook` (
-  `ebook_id` int(11) NOT NULL COMMENT 'รหัส E-Book (Primary Key)',
-  `title_id` int(11) DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
-  `ebook_file` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ไฟล์ ebook'
+  `ebook_id` int NOT NULL COMMENT 'รหัส E-Book (Primary Key)',
+  `title_id` int DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
+  `ebook_file` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ไฟล์ ebook'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : หนังสืออิเล็กทรอนิกส์';
 
 -- --------------------------------------------------------
@@ -69,11 +67,10 @@ CREATE TABLE `ebook` (
 -- Table structure for table `ebook_log`
 --
 
-DROP TABLE IF EXISTS `ebook_log`;
 CREATE TABLE `ebook_log` (
-  `ebooklog_id` int(11) NOT NULL COMMENT 'รหัสการเก็บ',
-  `ebook_id` int(11) DEFAULT NULL COMMENT 'รหัส E-book',
-  `member_id` int(11) DEFAULT NULL COMMENT 'รหัสสมาชิก',
+  `ebooklog_id` int NOT NULL COMMENT 'รหัสการเก็บ',
+  `ebook_id` int DEFAULT NULL COMMENT 'รหัส E-book',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก',
   `ebooklog_date` timestamp NULL DEFAULT NULL COMMENT 'วันเวลาที่เปิดอ่าน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : บันทึกการอ่าน E-Book';
 
@@ -83,10 +80,9 @@ CREATE TABLE `ebook_log` (
 -- Table structure for table `fine`
 --
 
-DROP TABLE IF EXISTS `fine`;
 CREATE TABLE `fine` (
-  `fine_id` int(11) NOT NULL COMMENT 'รหัสการปรับ',
-  `loan_id` int(11) DEFAULT NULL COMMENT 'รหัสการยืม',
+  `fine_id` int NOT NULL COMMENT 'รหัสการปรับ',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
   `fine_amount` decimal(8,2) DEFAULT NULL COMMENT 'จำนวนเงินที่ปรับ',
   `incurred_date` date DEFAULT NULL COMMENT 'วันที่เริ่มคิดค่าปรับ',
   `paid_date` datetime DEFAULT NULL COMMENT 'วันที่มาชำระเงิน'
@@ -98,15 +94,14 @@ CREATE TABLE `fine` (
 -- Table structure for table `loan`
 --
 
-DROP TABLE IF EXISTS `loan`;
 CREATE TABLE `loan` (
-  `loan_id` int(11) NOT NULL COMMENT 'รหัสการยืม',
-  `member_id` int(11) DEFAULT NULL COMMENT 'รหัสสมาชิก',
+  `loan_id` int NOT NULL COMMENT 'รหัสการยืม',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก',
   `loan_date` date DEFAULT NULL COMMENT 'วันที่ยืม',
   `due_date` date DEFAULT NULL COMMENT 'วันครบกำหนดคืน',
   `return_date` date DEFAULT NULL COMMENT 'วันที่คืนจริง',
-  `renewal_count` int(11) DEFAULT '0' COMMENT 'จำนวนครั้งที่ต่ออายุ',
-  `status` enum('borrow','receive','somereturn','return') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (borrow ยืม, receive รับหนังสือ, somereturn คืนแล้วบางส่วน return คืนหนังสือ)'
+  `renewal_count` int DEFAULT '0' COMMENT 'จำนวนครั้งที่ต่ออายุ',
+  `status` enum('borrow','receive','somereturn','return') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (borrow ยืม, receive รับหนังสือ, somereturn คืนแล้วบางส่วน return คืนหนังสือ)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : การยืม';
 
 -- --------------------------------------------------------
@@ -115,12 +110,11 @@ CREATE TABLE `loan` (
 -- Table structure for table `loan_item`
 --
 
-DROP TABLE IF EXISTS `loan_item`;
 CREATE TABLE `loan_item` (
-  `loan_item_id` int(11) NOT NULL COMMENT 'รหัสรายละเอียดการยืม',
-  `loan_id` int(11) DEFAULT NULL COMMENT 'รหัสการยืม',
-  `copy_id` int(11) DEFAULT NULL COMMENT 'รหัสเล่มหนังสือ',
-  `status` enum('borrow','return') COLLATE utf8mb4_unicode_ci DEFAULT 'borrow' COMMENT 'สถานะ (ยืม, คืน)',
+  `loan_item_id` int NOT NULL COMMENT 'รหัสรายละเอียดการยืม',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
+  `copy_id` int DEFAULT NULL COMMENT 'รหัสเล่มหนังสือ',
+  `status` enum('borrow','return') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'borrow' COMMENT 'สถานะ (ยืม, คืน)',
   `return_date` date DEFAULT NULL COMMENT 'วันที่คืน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : รายละเอียดการยืม';
 
@@ -130,12 +124,11 @@ CREATE TABLE `loan_item` (
 -- Table structure for table `loan_renewal`
 --
 
-DROP TABLE IF EXISTS `loan_renewal`;
 CREATE TABLE `loan_renewal` (
-  `renewal_id` int(11) NOT NULL COMMENT 'รหัสการต่ออายุ',
-  `loan_id` int(11) DEFAULT NULL COMMENT 'รหัสการยืม',
+  `renewal_id` int NOT NULL COMMENT 'รหัสการต่ออายุ',
+  `loan_id` int DEFAULT NULL COMMENT 'รหัสการยืม',
   `renewal_date` date DEFAULT NULL COMMENT 'วันที่ต่ออายุ',
-  `renewal_count_date` int(11) DEFAULT NULL COMMENT 'จำนวนวันที่ขอต่ออายุ',
+  `renewal_count_date` int DEFAULT NULL COMMENT 'จำนวนวันที่ขอต่ออายุ',
   `return_date` date DEFAULT NULL COMMENT 'วันที่ต้องคืน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : การต่ออายุ';
 
@@ -145,23 +138,22 @@ CREATE TABLE `loan_renewal` (
 -- Table structure for table `member`
 --
 
-DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
-  `member_id` int(11) NOT NULL COMMENT 'รหัสสมาชิก (PK)',
-  `emloyee_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสพนักงาน',
-  `first_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อจริง',
-  `last_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'นามสกุล',
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'อีเมล (ใช้ติดต่อ)',
-  `tel` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'เบอร์โทรติดต่อ',
-  `staus` enum('active','inactive') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''active'', ''inactive'', ''pending'')'
+  `member_id` int NOT NULL COMMENT 'รหัสสมาชิก (PK)',
+  `employee_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสพนักงาน',
+  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'ชื่อจริง',
+  `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'นามสกุล',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'อีเมล (ใช้ติดต่อ)',
+  `tel` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'เบอร์โทรติดต่อ',
+  `status` enum('active','inactive') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''active'', ''inactive'', ''pending'')'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : สมาชิก';
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`member_id`, `emloyee_id`, `first_name`, `last_name`, `email`, `tel`, `staus`) VALUES
-(1, '0000', 'ผู้ดูแลระบบ', 'เจ้าหน้าที่ห้องสมุด', 'admin@mail.com', '0999999999', 'active');
+INSERT INTO `member` (`member_id`, `employee_id`, `first_name`, `last_name`, `email`, `tel`, `status`) VALUES
+(1, '0000', 'เจ้าหน้าที่ดูแลห้องสมุด', 'ผู้ดูแลระบบ', 'admin@mail.com', '0999999999', 'active');
 
 -- --------------------------------------------------------
 
@@ -169,12 +161,11 @@ INSERT INTO `member` (`member_id`, `emloyee_id`, `first_name`, `last_name`, `ema
 -- Table structure for table `physical_copy`
 --
 
-DROP TABLE IF EXISTS `physical_copy`;
 CREATE TABLE `physical_copy` (
-  `copy_id` int(11) NOT NULL COMMENT 'รหัสฉบับเล่ม',
-  `title_id` int(11) DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
-  `accession_no` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'เลขทะเบียน/รหัสประจำเล่ม (ใช้ติดตามการยืม-คืน)',
-  `status` enum('available','on_loan','lost') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''available'', ''on_loan'', ''lost'')'
+  `copy_id` int NOT NULL COMMENT 'รหัสฉบับเล่ม',
+  `title_id` int DEFAULT NULL COMMENT 'รหัสชื่อเรื่อง',
+  `accession_no` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'เลขทะเบียน/รหัสประจำเล่ม (ใช้ติดตามการยืม-คืน)',
+  `status` enum('available','on_loan','lost') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'สถานะ (''available'', ''on_loan'', ''lost'')'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง :  หนังสือที่เป็นเล่ม';
 
 -- --------------------------------------------------------
@@ -183,15 +174,14 @@ CREATE TABLE `physical_copy` (
 -- Table structure for table `settings`
 --
 
-DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
-  `id` int(11) NOT NULL COMMENT 'รหัส',
-  `max_loan_days` int(11) DEFAULT NULL COMMENT 'วันที่ยืมได้สูงสุด',
-  `renew_days_before_due` int(11) DEFAULT NULL COMMENT 'ต่ออายุก่อนหมดอายุ',
-  `renew_days_after_due` int(11) DEFAULT NULL COMMENT 'ต่ออายุหลังหมดอายุ',
-  `max_renew_count` int(11) DEFAULT NULL COMMENT 'จำนวนครั้งยืมต่อสูงสุด',
-  `max_renew_days` int(11) DEFAULT NULL COMMENT 'จำนวนวันที่ต่ออายุได้สูงสุด',
-  `fine_per_day` int(11) DEFAULT NULL COMMENT 'ค่าปรับต่อวัน'
+  `id` int NOT NULL COMMENT 'รหัส',
+  `max_loan_days` int DEFAULT NULL COMMENT 'วันที่ยืมได้สูงสุด',
+  `renew_days_before_due` int DEFAULT NULL COMMENT 'ต่ออายุก่อนหมดอายุ',
+  `renew_days_after_due` int DEFAULT NULL COMMENT 'ต่ออายุหลังหมดอายุ',
+  `max_renew_count` int DEFAULT NULL COMMENT 'จำนวนครั้งยืมต่อสูงสุด',
+  `max_renew_days` int DEFAULT NULL COMMENT 'จำนวนวันที่ต่ออายุได้สูงสุด',
+  `fine_per_day` int DEFAULT NULL COMMENT 'ค่าปรับต่อวัน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : การตั้งค่าต่าง ๆ';
 
 --
@@ -207,13 +197,12 @@ INSERT INTO `settings` (`id`, `max_loan_days`, `renew_days_before_due`, `renew_d
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL COMMENT 'รหัสผู้ใช้งาน (PK)',
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ชื่อผู้ใช้สำหรับ Login',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสผ่านที่ถูกเข้ารหัส',
-  `role` enum('member','admin') COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'สิทธิ์การใช้งาน (''member'', ''admin'')',
-  `member_id` int(11) DEFAULT NULL COMMENT 'รหัสสมาชิก'
+  `user_id` int NOT NULL COMMENT 'รหัสผู้ใช้งาน (PK)',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ชื่อผู้ใช้สำหรับ Login',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'รหัสผ่านที่ถูกเข้ารหัส',
+  `role` enum('member','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'สิทธิ์การใช้งาน (''member'', ''admin'')',
+  `member_id` int DEFAULT NULL COMMENT 'รหัสสมาชิก'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ตาราง : ผู้ใช้ระบบ';
 
 --
@@ -289,7 +278,7 @@ ALTER TABLE `loan_renewal`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`member_id`),
-  ADD UNIQUE KEY `member_unique` (`emloyee_id`);
+  ADD UNIQUE KEY `member_unique` (`employee_id`);
 
 --
 -- Indexes for table `physical_copy`
@@ -321,73 +310,73 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `book_title`
 --
 ALTER TABLE `book_title`
-  MODIFY `title_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสชื่อเรื่อง (Primary Key)';
+  MODIFY `title_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสชื่อเรื่อง (Primary Key)';
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสหมวดหมู่ (Primary Key)';
+  MODIFY `category_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสหมวดหมู่ (Primary Key)';
 
 --
 -- AUTO_INCREMENT for table `ebook`
 --
 ALTER TABLE `ebook`
-  MODIFY `ebook_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัส E-Book (Primary Key)';
+  MODIFY `ebook_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัส E-Book (Primary Key)';
 
 --
 -- AUTO_INCREMENT for table `ebook_log`
 --
 ALTER TABLE `ebook_log`
-  MODIFY `ebooklog_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการเก็บ';
+  MODIFY `ebooklog_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการเก็บ';
 
 --
 -- AUTO_INCREMENT for table `fine`
 --
 ALTER TABLE `fine`
-  MODIFY `fine_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการปรับ';
+  MODIFY `fine_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการปรับ';
 
 --
 -- AUTO_INCREMENT for table `loan`
 --
 ALTER TABLE `loan`
-  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการยืม';
+  MODIFY `loan_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการยืม';
 
 --
 -- AUTO_INCREMENT for table `loan_item`
 --
 ALTER TABLE `loan_item`
-  MODIFY `loan_item_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสรายละเอียดการยืม';
+  MODIFY `loan_item_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสรายละเอียดการยืม';
 
 --
 -- AUTO_INCREMENT for table `loan_renewal`
 --
 ALTER TABLE `loan_renewal`
-  MODIFY `renewal_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการต่ออายุ';
+  MODIFY `renewal_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสการต่ออายุ';
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสสมาชิก (PK)', AUTO_INCREMENT=2;
+  MODIFY `member_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสสมาชิก (PK)', AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `physical_copy`
 --
 ALTER TABLE `physical_copy`
-  MODIFY `copy_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสฉบับเล่ม';
+  MODIFY `copy_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสฉบับเล่ม';
 
 --
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัส', AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัส', AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสผู้ใช้งาน (PK)', AUTO_INCREMENT=2;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'รหัสผู้ใช้งาน (PK)', AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
